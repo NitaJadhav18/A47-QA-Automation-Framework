4a82be19b53167aceef39425a3b6b7b512852ffb
NitaJadhav18 <nita.jadhav@testpro.io>
2023-07-06 23:09:06 -0500

    Homework-18

---
+++ src/test/java/Homework18.java
@@ -0,0 +1,18 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest{
+    @Test
+    public void playSong() throws InterruptedException {
+        openLoginUrl();
+        enterEmail("Nita.jadhav@testpro.io");
+        enterPassword("te$t$tudent");
+        clickSubmit();
+        //click playnext song button
+        clickPlaynextSong();
+        clickPlayButton();
+        Assert.assertTrue(checkSongPlaying());
+
+    }
+
+}
